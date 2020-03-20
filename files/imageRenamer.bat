d8344e38fa41415dd0a23dfcc42417da97c2251c
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-03-21 01:22:59 +0900

    Create imageRenamer.bat

---
+++ files/imageRenamer.bat
@@ -0,0 +1,16 @@
+@echo off
+
+rem This batch file rename photo (like: jpg, jpeg, png) to number.
+
+setlocal enabledelayedexpansion
+set num=1
+for /f "usebackq delims=" %%n in (`dir /a /b ^| findstr /i /r "^.*[jpg|jpeg|png]$"`) do (
+set numtmp=00!num!
+set numtmp=!numtmp:~-3,3!
+echo %%n to !numtmp!
+ren %%n !numtmp!.jpeg
+set /a num+=1
+)
+endlocal
+pause
+exit /b

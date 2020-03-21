e7afdd0d4b0ac711b1815cbc8a5317d87838316e
ikota3 <51883336+ikota3@users.noreply.github.com>
2020-03-21 11:11:44 +0900

    Update imageRenamer
    change regex.

--- files/imageRenamer.bat
+++ files/imageRenamer.bat
@@ -4,7 +4,7 @@ rem This batch file rename photo (like: jpg, jpeg, png) to number.
 
 setlocal enabledelayedexpansion
 set num=1
-for /f "usebackq delims=" %%n in (`dir /a /b ^| findstr /i /r "^.*[jpg|jpeg|png]$"`) do (
+for /f "usebackq delims=" %%n in (`dir /a /b ^| findstr /i /r "\.jpg$ \.jpeg$ \.png$"`) do (
 set numtmp=00!num!
 set numtmp=!numtmp:~-3,3!
 echo %%n to !numtmp!
